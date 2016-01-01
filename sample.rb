11d21510ec9484ea673897d8b6fc95b3306c8490
Brian Zeligson <bzeligson@localytics.com>
2015-12-31 21:46:17 -0500

    use better diff for sample

--- sample.rb
+++ sample.rb
@@ -3,7 +3,7 @@ require 'code_tour'
 t = CodeTour.define do
   version_control_integration CodeTour::Git
   renderer CodeTour::TextRenderer
-  block(diff('HEAD')) { "Have a diff" }
+  block(show('d9bff6dfb')) { "Have a diff" }
   block(static('HEAD', ['lib/code_tour/version.rb'])) do
     "And a version file"
   end
